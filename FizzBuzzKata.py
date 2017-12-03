aefe2fa5a5e3dfce54e0d99d56f939aea39c7319
rafalmajor <rafal.major@gmail.com>
2017-12-03 22:13:55 +0100

    Assert7

--- FizzBuzzKata.py
+++ FizzBuzzKata.py
@@ -2,16 +2,20 @@ import unittest
 
 
 def get_answer(number):
+    if number==15:
+        return "FizzBuzz"
     if number % 3==0:
         return "Fizz"
     elif number % 5== 0:
         return "Buzz"
+
     return  number
 
-
+print(get_answer(15))
 assert (get_answer(1)==1)
 assert (get_answer(2)==2)
 assert (get_answer(3)=="Fizz")
 assert (get_answer(6)=="Fizz")
 assert (get_answer(5)=="Buzz")
 assert (get_answer(10)=="Buzz")
+assert (get_answer(15)=="FizzBuzz")
