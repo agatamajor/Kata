a8950ceac22222bf384f54dd572e947d95c504a7
rafalmajor <rafal.major@gmail.com>
2017-12-03 22:06:52 +0100

    Assert4

--- FizzBuzzKata.py
+++ FizzBuzzKata.py
@@ -2,7 +2,7 @@ import unittest
 
 
 def get_answer(number):
-    if number ==3:
+    if number % 3==0:
         return "Fizz"
     return  number
 
@@ -10,3 +10,4 @@ def get_answer(number):
 assert (get_answer(1)==1)
 assert (get_answer(2)==2)
 assert (get_answer(3)=="Fizz")
+assert (get_answer(6)=="Fizz")
